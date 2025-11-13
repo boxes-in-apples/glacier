c2ac45305a91afba0ae67015b1df85507d15c1be
boxes-in-apples <georgiyromanovskiy@kingsway.college>
2025-11-12 21:34:47 -0500

    Пофиксил движок.cpp

--- модули/движок.cpp
+++ модули/движок.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 
-std::string menu() {
+
+std::string menu() { // Изменить тип возвращаемого значения, так как теперь функция управляет циклом
+
     std::cout << "GLACIER 0.1.1\nДобро пожаловать!\n" << std::endl;
-    while (true) {
-        std::cout << "Glacier>";
-        std::string command;
-        std::getline(std::cin, command);
-        return command;
-    }
+
+    std::cout << "Glacier>";
+
+    std::string command;
+    std::getline(std::cin, command);
+
+    return command;
+
 }

733e29841d1937ccbeb3819a5b68d96567cb56d2
boxes-in-apples <georgiyromanovskiy@kingsway.college>
2025-11-12 21:38:54 -0500

    Комментариев добавил в движок.cpp

--- модули/движок.cpp
+++ модули/движок.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-#include <string>
+//движок.cpp
+#include <iostream> //Это чтобы выходы работали библиотека
+#include <string> //А это чтобы строку вводить можно было
 
 
-std::string menu() { // Изменить тип возвращаемого значения, так как теперь функция управляет циклом
+std::string menu() { //функция меню
 
-    std::cout << "GLACIER 0.1.1\nДобро пожаловать!\n" << std::endl;
+    std::cout << "GLACIER 0.1.1\nДобро пожаловать!\n" << std::endl; //Первая линия в меню
 
-    std::cout << "Glacier>";
+    std::cout << "Glacier>"; //Ввод сюда будет
 
-    std::string command;
-    std::getline(std::cin, command);
+    std::string command; //создаем строку-переменную для команды ну это понятно
+    std::getline(std::cin, command); //получаем целую линию в переменную команда
 
-    return command;
+    return command; //возвращаем команду
 
 }

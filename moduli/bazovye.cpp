7bb49a922c2eb98d4d683f92fcdb4897dbb22a54
boxes-in-apples <georgiyromanovskiy@kingsway.college>
2025-11-16 18:57:30 -0500

    Create bazovye.cpp

---
+++ moduli/bazovye.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include "h/bazovye.h"
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int tokenize() {
+    std::cout << "We tokenize it\n";
+    std::string command;
+    std::cin >> command;
+    std::cout << command;
+
+    return 0;
+}

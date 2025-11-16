084ade320ca97f295b79e8334e50b6d24c34edb1
boxes-in-apples <georgiyromanovskiy@kingsway.college>
2025-11-16 18:22:51 -0500

    Create Glacier.cpp

---
+++ Glacier.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Welcome to Glacier 0.1.1 C++ version.\nGlacier>>>";
+    std::string command;
+    std::cin >> command;
+    std::cout << command;
+
+    return 0;
+}

159e177bb949ebd5e9fa27ba221d8437b668b134
boxes-in-apples <georgiyromanovskiy@kingsway.college>
2025-11-22 22:36:04 -0500

    Update Glacier.cpp

--- Glacier.cpp
+++ Glacier.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 
+int tokenize(const std::string & string);
+
 int main() {
     std::cout << "Welcome to Glacier 0.1.1 C++ version.\nGlacier>>>";
     std::string command;
-    std::cin >> command;
-    std::cout << command;
+    std::getline(std::cin, command);
 
+    tokenize(command);
     return 0;
 }
